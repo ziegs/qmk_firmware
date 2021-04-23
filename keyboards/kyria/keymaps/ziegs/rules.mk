621b0d91be8d360f2a974ccef5fb2572b4ef9b89
Matthew Ziegelbaum <ziegs@google.com>
2021-04-23 12:15:11 -0400

    Add ziegs keymap for Kyria

---
+++ keyboards/kyria/keymaps/ziegs/rules.mk
@@ -0,0 +1,5 @@
+OLED_DRIVER_ENABLE = no   # Disables the use of OLED displays
+RGBLIGHT_ENABLE = yes # Enable keyboard RGB underglow
+MAGIC_ENABLE = no
+SPACE_CADET_ENABLE = no
+GRAVE_ESC_ENABLE = no
