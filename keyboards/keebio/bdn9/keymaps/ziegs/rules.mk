ab5899a86bf521f96f76f908c577f9fc1a692bef
Matthew Ziegelbaum <ziegs@google.com>
2021-04-23 12:10:51 -0400

    Start a BDN9 layout

---
+++ keyboards/keebio/bdn9/keymaps/ziegs/rules.mk
@@ -0,0 +1,11 @@
+# Use macropad-specific defines, layers, functions. Flash bootloader with QM_MAKE
+IS_MACROPAD = yes
+
+# MCU name for Elite-C
+MCU = atmega32u4
+
+# Use dfu bootloader for Elite-C
+BOOTLOADER = atmel-dfu
+
+# Build Options
+TAP_DANCE_ENABLE = yes
